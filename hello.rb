72c7f782405fb547e80cafe39d5e6bb100ef56e2
neo972 <pablo972fox@gmail.com>
2017-01-09 15:07:04 +0100

    git add numbers.rb

--- hello.rb
+++ hello.rb
@@ -3,3 +3,6 @@ puts "Quel est votre nom ?"
 nom = gets.chomp
 puts "Bonjour #{nom}"
 puts "Comment vas tu ?"
+3.times do
+    puts "Hello #{nom}"
+    end
---
+++ numbers.rb
@@ -0,0 +1,11 @@
+# Exercices sur les nombres
+# Calculez le nombre de seconde dans une année
+
+minute = 60
+hour = 60
+day = 24 
+week = 7
+an = 365
+
+un_an = (minute * hour * day) * an
+puts un_an
