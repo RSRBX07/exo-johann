7b31d637838c2058829949a37e4953ce08e1ce01
neo972 <pablo972fox@gmail.com>
2017-01-09 15:22:18 +0100

    git add numbers.rb

--- numbers.rb
+++ numbers.rb
@@ -1,11 +1,25 @@
 # Exercices sur les nombres
 # Calculez le nombre de seconde dans une année
 
-minute = 60
-hour = 60
-day = 24 
-week = 7
-an = 365
+# minute = 60
+# hour = 60
+# day = 24 
+# week = 7
+# year = 365
+# age = 39
+# year_b = 1977
+# day_b = 27
+# month_b = 9
 
-un_an = (minute * hour * day) * an
-puts un_an
+
+# one_year = (minute * hour * day) * year
+# puts one_year
+
+# age_in_s = age * one_year
+# puts age_in_s
+
+
+age = Time.now - Time.new(1977, 9, 27, 16, 0)
+puts age
+origine = Time.new(0)
+puts origine
