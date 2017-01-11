d27d58dcb1305e0642042d7006bb4058510fdcee
neo972 <pablo972fox@gmail.com>
2017-01-11 12:25:19 +0100

    le nouvo

--- lotto.rb
+++ lotto.rb
@@ -1,6 +1,10 @@
-mon_tab = []
+#demander une grille de jeu
 
-5.times do
-    mon_tab[rand 5]
+#affichage du montant de la cagnote
+#entre 100 et 500.000 Euros
+# le vendredi 13, la cagnote est de 2 millions
 
-    end
+# puts "Entrez 5 numéros"
+# ma_grille = gets.chomp
+ma_grille = [1,2,3,4,5]
+tirage = (1..49).shuffle.sort.take 5
