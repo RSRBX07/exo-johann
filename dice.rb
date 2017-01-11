2c5b4df428b18a698a7a2be411c2a2796986adcb
neo972 <pablo972fox@gmail.com>
2017-01-11 17:01:19 +0100

    new

--- dice.rb
+++ dice.rb
@@ -1,9 +1,22 @@
 
 
 
-def roll
-  de = [1,2,3,4,5,6]
-  rolled = de[rand 6]
+# def roll
+#   de = [1,2,3,4,5,6]
+#   rolled = de[rand 6]
+# end
+
+# def roll_cheated de
+#   return de
+# end
+
+def roll(cheated_value = nil)
+  if cheated_value
+    return cheated_value
+  else
+    return 1 + rand(6)
+  end
 end
 
-puts roll
+puts roll 
+puts roll 5
