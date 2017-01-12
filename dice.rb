5c8c8f4736082f8b18c1400eded7b47c78842faa
neo972 <pablo972fox@gmail.com>
2017-01-12 11:48:10 +0100

    project clean

--- dice.rb
+++ dice.rb
@@ -1,5 +1,31 @@
+# meta = {
+#   "place"     => "Roubaix",
+#   "author"    => "Johann"
+# }
+# place = meta["place"]
+# author = meta["author"]
+# puts "Made with love @ #{place} by #{author}"
+
+code_postaux = {
+  "Lille"       => 59000,
+  "Martinique"  => 97200,
+  "Guadeloupe"  => 97100
+}
+
+puts "Entrez la ville pour avoir le code postal :"
+city = gets.chomp
+city = city.capitalize!
+found=false
+code_postaux.each do |key, value|
+  if city == key
+    puts "Le code postal de votre ville est : #{value}"
+    found=true
+    break
+  end      
+end
 
 
+puts "Cette ville ne fais pas partie de mon systeme, désolé" unless found
 
 # def roll
 #   de = [1,2,3,4,5,6]
@@ -10,13 +36,14 @@
 #   return de
 # end
 
-def roll(cheated_value = nil)
-  if cheated_value
-    return cheated_value
-  else
-    return 1 + rand(6)
-  end
-end
+# def roll(cheated_value = nil)
+#   if cheated_value
+#     return cheated_value
+#   else
+#     return 1 + rand(6)
+#   end
+# end
 
-puts roll 
-puts roll 5
+# puts roll 
+# puts roll 5
+
