5f7215b97437f2d4e8fb87f4bc7851de56e3e88d
neo972 <pablo972fox@gmail.com>
2017-01-12 14:33:54 +0100

    version 1

--- french_number.rb
+++ french_number.rb
@@ -1,7 +1,32 @@
 # ecrire une methode
 def number(num)
-return num.to_s
+    case num
+    when 0
+    "zero"
+  when 1
+    "un"
+  when 2
+    "zero"
+  when 3
+    "trois"
+  when 4
+    "quattre"
+  when 5
+    "cinq"
+  when 6
+    "six"
+  when 7
+    "sept"
+  when 8
+    "huit"
+  when 9
+    "neuf"
+  else
+    nil
+  end
 end
+
+puts in_french 5
 
 # qui prend une instance de Fixum en argument
 # qui retourne la chaine de charactere du nombre en toute lettre
