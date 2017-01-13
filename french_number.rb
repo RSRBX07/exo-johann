0334f29a2a2fc508ed687ceee0b7fa6550fd351a
neo972 <pablo972fox@gmail.com>
2017-01-13 11:02:45 +0100

    the last to finish

--- french_number.rb
+++ french_number.rb
@@ -1,6 +1,7 @@
 # ecrire une methode
 def number(num)
  nmbrs = ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze","treize","quatorze","quinze","seize","dix-sept","dix-huit","dix-neuf"]
+ nmbrs_dizaine = []
  nmbrs[num] if num < 20
 end
 
