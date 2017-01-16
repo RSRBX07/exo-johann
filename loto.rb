a18f8114acb07a5606b360f021b33d51fd94efa5
neo972 <pablo972fox@gmail.com>
2017-01-16 16:17:56 +0100

    work in progress

--- loto.rb
+++ loto.rb
@@ -1,14 +1,76 @@
-
-
+require 'date'
 class Loto
-  # une grille pour le loto courant
-  def grid_validation grid
-    i = 1
-    @my_grid ||= []
-    @my_grid << grid + "#{i}"
-    i += 1
+
+  def self.get_grid
+    grid = []
+    5.times do
+      input = gets.to_i
+      grid << input
+    end
+    grid
   end
 
-def 
+  def self.get_flash
+    (1..45).to_a.shuffle.take 5
+  end
 
+  def has_winner?
+    #comprer tous les bulletins valides avec la grille gagnante
+    sorted_draw = draw.sort
+    @saved_grids.each do |grid|
+      sorted_grid = grid.sort
+      return true if sorted_grid == sorted_draw
+    end
+    return false
+  end
+
+  # enregistre une grille
+  # pour le loto courant
+  def validate_grid grid
+    # verifier que le tirage n'a pas encore eu lieux
+    "Vous pouvez encore jouer" if draw().empty?
+    @saved_grids ||= []
+    @saved_grids.push grid
+    
+  end 
+  # demander une grille de jeu
+
+  # affichage du montant de la cagnote
+  # entre 100 et 500.000 Euros
+  # le vendredi 13, la cagnote est de 2 millions
+  def vendredi_13?
+    Date.today.day == 13 && Date.today.friday?
+  end
+
+  
+
+  def prize
+    cagnote = if vendredi_13?
+        2_000_000
+      else
+        100_000
+      end
+    puts "Le montant de la cagnote du jour est de #{cagnote}"
+    cagnote
+  end
+
+  def draw
+    available_balls = (1..45).to_a
+    # shuffle balls and take 5
+    # @picked_balls ||= available_balls.shuffle.take(5)
+    @picked_balls = @picked_balls || available_balls.shuffle.take 5
+
+    puts "Le tirage du jour est : #{@picked_balls.sort}" 
+    @picked_balls
+  end
+
+  def check_grid grid
+    # afficher si gagne ou perdu
+    if grid.sort == draw.sort
+      puts "You win !"
+    else
+      puts "You loose !"
+    end
+  end
+  
 end
