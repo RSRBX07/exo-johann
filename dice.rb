e3bddcfe388320c9cbee168b8a573c69580d1df8
neo972 <pablo972fox@gmail.com>
2017-01-16 10:31:41 +0100

    remove methods in test.rb

--- dice.rb
+++ dice.rb
@@ -6,48 +6,42 @@
 # author = meta["author"]
 # puts "Made with love @ #{place} by #{author}"
 
-code_postaux = {
-  Lille:      59000,
-  Martinique: 97200,
-  Guadeloupe: 97100,
-  Mars:       112345
-}
+# code_postaux = {
+#   Lille:      59000,
+#   Martinique: 97200,
+#   Guadeloupe: 97100,
+#   Mars:       112345
+# }
 
-puts "Entrez la ville pour avoir le code postal :"
-city = gets.chomp
-city = city.capitalize!
-city = city.to_sym
+# puts "Entrez la ville pour avoir le code postal :"
+# city = gets.chomp
+# city = city.capitalize!
+# city = city.to_sym
 
-found=false
-code_postaux.each do |key, value|
-  if city == key
-    puts "Le code postal de votre ville est : #{value}"
-    found=true
-    break
-  end      
-end
-
-
-puts "Cette ville ne fais pas partie de mon systeme, désolé" unless found
-
-# def roll
-#   de = [1,2,3,4,5,6]
-#   rolled = de[rand 6]
+# found=false
+# code_postaux.each do |key, value|
+#   if city == key
+#     puts "Le code postal de votre ville est : #{value}"
+#     found=true
+#     break
+#   end      
 # end
 
-# def roll_cheated de
-#   return de
-# end
 
-# def roll(cheated_value = nil)
-#   if cheated_value
-#     return cheated_value
-#   else
-#     return 1 + rand(6)
-#   end
-# end
+# puts "Cette ville ne fais pas partie de mon systeme, désolé" unless found
 
-# puts roll 
-# puts roll 5
 
 class Dice
+  def roll_cheated de
+    return de
+  end
+
+  def roll(cheated_value = nil)
+    if cheated_value
+      return cheated_value
+    else
+      return 1 + rand(6)
+    end
+  end
+
+end
