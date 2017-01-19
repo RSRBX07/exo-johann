76972e556e0977c74a0361dd87f895c6fb161b0b
neo972 <pablo972fox@gmail.com>
2017-01-19 11:36:54 +0100

    Ne pas faire de variable de classe

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,9 +1,27 @@
+class Counter
+  attr_reader :value
+  def initialize
+    @value = 0
+  end
+  def add_one
+    @value += 1
+  end
+
+end
 class Vehicle
   attr_reader :position
   attr_reader :out_of_order
+  
 
-  def initialize    
+  def self.count
+    counter = Counter.new
+    counter.value
+  end
+
+  def initialize
     @out_of_order = false
+    counter = Counter.new
+    counter.add_one
   end
 
   def move destination
@@ -13,5 +31,7 @@ class Vehicle
 
 end
 
+puts Vehicle.count
 firetruck = Vehicle.new
+puts Vehicle.count
 
