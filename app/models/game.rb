29823d991aa202f261511932dc909c0e5eef8a76
neo972 <pablo972fox@gmail.com>
2017-01-23 16:49:26 +0100

    42

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,20 @@
 class Game < ApplicationRecord
+
+  def add_player
+    @players += 1
+  end
+
+  def remov_player
+    return @players if players <= 0
+      @players -= 1
+    else
+      @players
+  end
+
+    private
+
+  def initialyze
+    @players = 0
+  end
+
 end
